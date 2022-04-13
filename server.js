75d72c17712ab572d87557fd3f340f57195d5cc1
Carlos Carbonell <carlos.carbonell@geniussports.com>
2022-04-13 17:06:46 +0300

    Tweaking server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json())
 // Setup static directory to serve
 app.use(express.static(path.resolve('client', 'build')))
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.resolve('client', 'build', 'index.html'))
 })
 
